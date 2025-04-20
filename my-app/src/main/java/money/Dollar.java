b57a040eae580610be67f1b4b0d783b66b823b68
Wakamatu57 <raamentaishimasshu@gmail.com>
2025-04-20 15:57:50 +0900

    コンストラクタを親に引き上げる

--- my-app/src/main/java/money/Dollar.java
+++ my-app/src/main/java/money/Dollar.java
@@ -2,10 +2,8 @@ package money;
 
 public class Dollar extends Money {
     Dollar(int amount, String currency) {
-        this.amount = amount;
-        this.currency = currency;
+        super(amount, currency);
     }
-
     Money times(int multipliction) {
         return Money.dollar(this.amount * multipliction);
     }
