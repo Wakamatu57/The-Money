55d94ca1423818729d92ac8fb40c4438ea9b6365
Wakamatu57 <raamentaishimasshu@gmail.com>
2025-04-21 18:21:41 +0900

    testReduceMoneyDifferentCurrencyに対するリファクタリング

--- my-app/src/main/java/money/Bank.java
+++ my-app/src/main/java/money/Bank.java
@@ -1,15 +1,22 @@
 package money;
 
+import java.util.Map;
+import java.util.HashMap;
+
 public class Bank {
+    private Map<Pair, Integer> rates = new HashMap<>();
     Money reduced(Expression source, String to) {
         return source.reduced(this, to);
 
     }
     void addRate(String from, String to, int rate) {
-
+        rates.put(new Pair(from, to), rate);
     }
 
     int rate(String from, String to) {
-        return (from.equals("CHF") && to.equals("USD")) ? 2: 1;
+        if (from.equals(to)) {
+            return 1;
+        }
+        return rates.get(new Pair(from, to));
     }
 }
